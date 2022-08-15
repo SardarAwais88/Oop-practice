7a75bb7fb613203241bf526889aff971f40712d0
Sardar <Sardar@DESKTOP-ON4BVQI>
2022-08-14 23:20:37 -0700

    Add project files.

---
+++ Oop pracice/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// instance of the classs or object
+Employee em1 = new Employee();
+Employee em2 = new Employee();
+em1.name = "sardar";
+em2.name = "Awais";
+
+em1.Address = "trarkhel";
+em2.Address = "nerian";
+// it is class blue print
+public class Employee
+{
+    public string name { get; set; }
+    public string Address { get; set; }
+}
