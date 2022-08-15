51c2406d5fb755f07e8b81cef938d65ce2f1887e
Sardar <Sardar@DESKTOP-ON4BVQI>
2022-08-15 10:16:19 -0700

    method overloading overriding

---
+++ overloadingoverriding/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+ method overloading means same
+
+method name with different signatures
+in the same class
+ 
+ */
+Manager m;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public string Address { get; set;}  
+}
+
+public class Manager: Employee
+{
+    // overriding
+    public void Management()
+    {
+
+    }
+    // same method name with different 
+    // signature
+    public void Management(int id)
+    {
+
+    }
+
+    // same method name with different 
+    // signature
+    public void Management(string Name)
+    {
+
+    }
+}
