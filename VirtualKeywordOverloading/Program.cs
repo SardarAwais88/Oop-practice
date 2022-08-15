c5f9ffb1567e4f127527c66452e0025291730316
Sardar <Sardar@DESKTOP-ON4BVQI>
2022-08-15 10:03:13 -0700

    virtual keyword is used to to define some logic in parent class and overriden in child class also

--- VirtualKeywordOverloading/Program.cs
+++ VirtualKeywordOverloading/Program.cs
@@ -1,2 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// 
+
+/*
+ virtual keyword helps us to define some logic 
+in the parent class 
+which can be overriden in child class also
+ */
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Explain inheritance
+// inheritance defines
+// a parent child relationship
+Manager m;
+public class Employee
+{
+    public string name { get; set; }
+    public string Address { get; set; }
+
+
+    public virtual void validate()
+    {
+        checkname();
+        checkaddress();
+    }
+
+
+    private void checkname()
+    {
+
+    }
+    private void checkaddress()
+    {
+
+    }
+}
+
+// manager class inherit Employee 
+// so it can access its: properties
+public class Manager : Employee
+{
+ 
+    // overriden in child class also
+    public virtual void validate()
+    {
+
+    }
+    public void Management()
+    {
+        manageclass();
+        ManageStudent();
+    }
+
+    private void manageclass()
+    {
+
+    }
+
+    public void ManageStudent()
+    {
+
+    }
+}
