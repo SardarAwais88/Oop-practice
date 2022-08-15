b89535a0c5cdcfc871a8b1f74f7f4804e80aa7ac
Sardar <Sardar@DESKTOP-ON4BVQI>
2022-08-15 00:03:22 -0700

    VirtualKeywordOverriding

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,2 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Explain inheritance
+// inheritance defines
+// a parent child relationship
+Manager m;
+public class Employee
+{
+    public string name { get; set; }
+    public string Address { get; set; }
+
+     
+    public void validate()
+    {
+        checkname();
+        checkaddress();
+    }
+
+
+    private void checkname()
+    {
+
+    }
+    private void checkaddress()
+    {
+
+    }
+}
+
+// manager class inherit Employee 
+// so it can access its: properties
+public class Manager : Employee
+{
+    public void Management()
+    {
+        manageclass();
+        ManageStudent();
+    }
+
+    private void manageclass()
+    {
+
+    }
+
+    public void ManageStudent()
+    {
+
+    }
+    }
